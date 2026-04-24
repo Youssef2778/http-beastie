b237832dc909c718fa2cda42f2799065f49b4a5d
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2026-04-25 02:46:22 +0300

    Add main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <boost/asio.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <iostream>
+#include <string>
+
+#include "utils.hpp"
+
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
+
+int main() {
+  try {
+    net::io_context ioc;
+    tcp::acceptor acceptor(ioc, tcp::endpoint(tcp::v4(), 8080));
+
+    std::cout << "Listening on http://localhost:8080\n";
+
+    while (true) {
+      tcp::socket socket(ioc);
+      acceptor.accept(socket);
+
+      beast::flat_buffer buf;
+      http::request<http::string_body> req;
+      http::read(socket, buf, req);
+
+      auto res = handle_request(req);
+      http::write(socket, res);
+
+      beast::error_code ec;
+      socket.shutdown(tcp::socket::shutdown_send, ec);
+    }
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << '\n';
+  }
+}
